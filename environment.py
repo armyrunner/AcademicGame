02416c3c3fe26d24efe893434ea7c4d58bfc6f12
Wesley Schmitt <d00223876@dmail.dixie.edu>
2020-11-13 11:19:02 -0700

    Scene initiates Window

--- environment.py
+++ environment.py
@@ -1,8 +1,8 @@
 import pygame
 
-class Scene(width, height):
+class Scene(width, height, bg_color = pygame.Color(r, g, b)):
 	"""docstring for Scene"""
 	def __init__(self, width, height, bg_color):
 		pygame.display.init()
 		self.window = pygame.display.set_mode(size(width, height))
-		self.window.fill(fill_color = pygame.Color(r, g, b))
+		self.window.fill(bg_color)
