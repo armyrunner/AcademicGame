4ec11efd159b6ec6dc28191607abc7be75915746
Kendra Richards <D00357535@dixie.edu>
2020-11-13 10:59:46 -0700

    set up base case for characters

--- character.py
+++ character.py
@@ -1,3 +1,34 @@
 class Character: 
     
-    def __init__(self):
+    def __init__(self,x,y,w,h):
+        self.mX = x
+        self.mY = y
+        self.mW = w
+        self.mH = h
+    
+    def getX():
+        return self.mX
+    
+    def setX(x):
+        self.mX = x
+      
+    def getY():
+        return self.mY
+    
+    def setY(y):
+        self.mY = y
+      
+    def getW():
+        return self.mW
+    
+    def setW(w):
+        self.mW = w
+      
+    def getH():
+        return self.mH
+    
+    def setH(h):
+        self.mH = h
+        
+        
+        
