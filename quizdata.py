3def2eb0d79525508bb01ca585d39f7b19f2883d
kally1009 <d00372177@dmail.dixie.edu>
2020-11-13 12:38:59 -0700

    updated quizdata

---
+++ quizdata.py
@@ -0,0 +1,51 @@
+class QuizData:
+
+    def __init__(self):
+        self.English={}
+        self.Geography={}
+        self.Math={}
+        self.History={}
+        self.Science={}
+        
+    #English
+    self.English["What is missing in this sentence? the dog is black and white."]="capital letter"
+    self.English["Which word has a short /e/ sound? a.bad b.bee c.bed"]=c
+    self.Enlgish["Choose the correct spelling. a.famous b.famus c.famos"] =a
+    self.English["Choose the past tense verb for this sentence. My mom ____(make) tacos on Tuesday. a.made b.makes c.is making d.make"]=a
+    self.Enlgish["Choose the correct homophone. I would like another _____ of cake. a.piece b.peace c.peece d.peez"]=a
+    self.English["We had a great day at the beach. a.Fact b.Opinion"]=a
+    self.English["Which sentence shows correct subject and predicate agreement? a.Lizards and snakes lives in Flordia. b.Lizards and snakes live in Flordia. c.Mary sing like a bird. d.The cheerleaders walks in the parades."]=b
+    
+    #Geography
+    self.Geography["Where is the capital of Utah? a.Salt Lake City b. Salt Lake Town c. St.George d.Provo"]=a
+    self.Geography["What is the abbrevation for Navada? a.ND b.NV c.NA d.NN"]=b
+    self.Geography["Where is the capital of the United States? a.Washington State b.Washington Utah c.Washington County d.Wahington D.C"]=d
+    self.Geography["What state is in the North-West corner of the United States? a.California b.Washington c.Oregan d.Utah"]=b
+    self.Geography["Where in Utah is Dixie State University? a.St.George b.Cedar City c.Provo d.Salt Lake City"]=a
+    self.Geography["What country does London reside in? a.France b.Germany c.England d.Ireland"]=c
+    self.Geography["Which country is next to England? a.Russia b.China c.Spain d.Ireland"]=d
+    
+    #math
+    self.Math["What is 200+70+1?"]=271
+    self.Math["What is 100-50?"]=50
+    self.Math["What is 3*7?"]=21
+    self.Math["What is 12*12?"]=144
+    self.Math["What is 3*(4+4)-7?"]=17
+    self.Math["What is 9*9?"]=81
+    
+    #History
+    self.History["Who was the first president of the United States? a.Abraham Lincoln b.Thomas Jefferson c.George Washington d.Donald Trump"]=c
+    self.History["Who is the president of the united states as of 2020? a.Joe Biden b.Donald Trump c.Barack Obama d.George Washington"]=b
+    self.History["Which modern day state was Jamestown located? a.Massachusetts b.New York c.Virginia d.Flordia"]=c
+    self.History["All of the original 13 colonies were British. a.True b.False"]=a
+    self.History["The statue of Liberty was a gift from which country? a.England b.Germany c.Russia d.France"]=d
+    self.History["What day is Independance day in the United States? a.July 1 b.June 4 c.July 4 d.June 1"]=c
+    
+    #Science
+    self.Science["What is the center of the atom called? a.electron b.proton c.neutron d.nucleus"]=d
+    self.Science["What force do you use to move a shopping cart forward? a.push b.pull"]=a
+    self.Science["What planet do we live on? a.Mars b.Earth c.Mercury d.The sun"]=b
+    self.Science["Milk is an example of what state of matter? a.Solid b.Gas c.Liquid d.Water"]=c
+    
+    
+            
