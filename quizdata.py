8af09ec69b04441aa354e6b3830408bd6a8cbdc0
kally1009 <d00372177@dmail.dixie.edu>
2020-11-13 17:01:09 -0700

    Fixed intentation error

--- quizdata.py
+++ quizdata.py
@@ -8,45 +8,45 @@ class QuizData:
         self.Science={}
         
     #English
-    self.English["What is missing in this sentence? the dog is black and white."]=("a","a.capital letter b.period c.red")
-    self.English["Which word has a short /e/ sound? "]=('c',"a.bad b.bee c.bed" )
-    self.Enlgish["Choose the correct spelling. "] =("a","a.famous b.famus c.famos")
-    self.English["Choose the past tense verb for this sentence. My mom ____(make) tacos on Tuesday." ]=("a","a.made b.makes c.is making d.make")
-    self.Enlgish["Choose the correct homophone. I would like another _____ of cake." ]=("a","a.piece b.peace c.peece d.peez")
-    self.English["We had a great day at the beach. a.Fact b.Opinion"]=a
-    self.English["Which sentence shows correct subject and predicate agreement? "]=("b","a.Lizards and snakes lives in Flordia. b.Lizards and snakes live in Flordia. c.Mary sing like a bird. d.The cheerleaders walks in the parades.")
+        self.English["What is missing in this sentence? the dog is black and white."]=("a","a.capital letter b.period c.red")
+        self.English["Which word has a short /e/ sound? "]=('c',"a.bad b.bee c.bed" )
+        self.Enlgish["Choose the correct spelling. "] =("a","a.famous b.famus c.famos")
+        self.English["Choose the past tense verb for this sentence. My mom ____(make) tacos on Tuesday." ]=("a","a.made b.makes c.is making d.make")
+        self.Enlgish["Choose the correct homophone. I would like another _____ of cake." ]=("a","a.piece b.peace c.peece d.peez")
+        self.English["We had a great day at the beach. a.Fact b.Opinion"]=a
+        self.English["Which sentence shows correct subject and predicate agreement? "]=("b","a.Lizards and snakes lives in Flordia. b.Lizards and snakes live in Flordia. c.Mary sing like a bird. d.The cheerleaders walks in the parades.")
     
     #Geography
-    self.Geography["Where is the capital of Utah?"]=("a"," a.Salt Lake City b. Salt Lake Town c. St.George d.Provo")
-    self.Geography["What is the abbrevation for Navada?" ]=("b","a.ND b.NV c.NA d.NN")
-    self.Geography["Where is the capital of the United States? "]=("d","a.Washington State b.Washington Utah c.Washington County d.Wahington D.C")
-    self.Geography["What state is in the North-West corner of the United States? "]=("b","a.California b.Washington c.Oregan d.Utah")
-    self.Geography["Where in Utah is Dixie State University? "]=("a","a.St.George b.Cedar City c.Provo d.Salt Lake City")
-    self.Geography["What country does London reside in? "]=("c","a.France b.Germany c.England d.Ireland")
-    self.Geography["Which country is next to England? "]=("d","a.Russia b.China c.Spain d.Ireland")
+        self.Geography["Where is the capital of Utah?"]=("a"," a.Salt Lake City b. Salt Lake Town c. St.George d.Provo")
+        self.Geography["What is the abbrevation for Navada?" ]=("b","a.ND b.NV c.NA d.NN")
+        self.Geography["Where is the capital of the United States? "]=("d","a.Washington State b.Washington Utah c.Washington County d.Wahington D.C")
+        self.Geography["What state is in the North-West corner of the United States? "]=("b","a.California b.Washington c.Oregan d.Utah")
+        self.Geography["Where in Utah is Dixie State University? "]=("a","a.St.George b.Cedar City c.Provo d.Salt Lake City")
+        self.Geography["What country does London reside in? "]=("c","a.France b.Germany c.England d.Ireland")
+        self.Geography["Which country is next to England? "]=("d","a.Russia b.China c.Spain d.Ireland")
     
     
     #math
-    self.Math["What is 200+70+1?"]=("a", "a.271 b.270 c. 269 d.272")
-    self.Math["What is 100-50?"]=("b","a.25 b.50 c.55 d.30")
-    self.Math["What is 3*7?"]=("c","a.20 b.22 c.21 d.30")
-    self.Math["What is 12*12?"]=("d","a.145 b.143 c.150 d.144")
-    self.Math["What is 3*(4+4)-7?"]=("a","a.17 b.18 c.9 d.3")
-    self.Math["What is 9*9?"]=("b","a.82 b.81 c.80 d.82")
+        self.Math["What is 200+70+1?"]=("a", "a.271 b.270 c. 269 d.272")
+        self.Math["What is 100-50?"]=("b","a.25 b.50 c.55 d.30")
+        self.Math["What is 3*7?"]=("c","a.20 b.22 c.21 d.30")
+        self.Math["What is 12*12?"]=("d","a.145 b.143 c.150 d.144")
+        self.Math["What is 3*(4+4)-7?"]=("a","a.17 b.18 c.9 d.3")
+        self.Math["What is 9*9?"]=("b","a.82 b.81 c.80 d.82")
     
     #History
-    self.History["Who was the first president of the United States? "]=("c","a.Abraham Lincoln b.Thomas Jefferson c.George Washington d.Donald Trump")
-    self.History["Who is the president of the united states as of 2020? "]=("b","a.Joe Biden b.Donald Trump c.Barack Obama d.George Washington")
-    self.History["Which modern day state was Jamestown located? "]=("c","a.Massachusetts b.New York c.Virginia d.Flordia")
-    self.History["All of the original 13 colonies were British. "]=("a","a.True b.False")
-    self.History["The statue of Liberty was a gift from which country? "]=("d","a.England b.Germany c.Russia d.France")
-    self.History["What day is Independance day in the United States? "]=("c","a.July 1 b.June 4 c.July 4 d.June 1")
+        self.History["Who was the first president of the United States? "]=("c","a.Abraham Lincoln b.Thomas Jefferson c.George Washington d.Donald Trump")
+        self.History["Who is the president of the united states as of 2020? "]=("b","a.Joe Biden b.Donald Trump c.Barack Obama d.George Washington")
+        self.History["Which modern day state was Jamestown located? "]=("c","a.Massachusetts b.New York c.Virginia d.Flordia")
+        self.History["All of the original 13 colonies were British. "]=("a","a.True b.False")
+        self.History["The statue of Liberty was a gift from which country? "]=("d","a.England b.Germany c.Russia d.France")
+        self.History["What day is Independance day in the United States? "]=("c","a.July 1 b.June 4 c.July 4 d.June 1")
     
     #Science
-    self.Science["What is the center of the atom called? "]=("d","a.electron b.proton c.neutron d.nucleus")
-    self.Science["What force do you use to move a shopping cart forward? "]=("a","a.push b.pull")
-    self.Science["What planet do we live on? "]=("b","a.Mars b.Earth c.Mercury d.The sun")
-    self.Science["Milk is an example of what state of matter? "]=("c","a.Solid b.Gas c.Liquid d.Water")
+        self.Science["What is the center of the atom called? "]=("d","a.electron b.proton c.neutron d.nucleus")
+        self.Science["What force do you use to move a shopping cart forward? "]=("a","a.push b.pull")
+        self.Science["What planet do we live on? "]=("b","a.Mars b.Earth c.Mercury d.The sun")
+        self.Science["Milk is an example of what state of matter? "]=("c","a.Solid b.Gas c.Liquid d.Water")
     
     
             
