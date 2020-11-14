5155f0b158dfdd36b1ad84b38636ccdcb095c56f
armyrunner <live2runmarathon@gmail.com>
2020-11-13 19:29:11 -0700

    add actionscene

--- academicgame.py
+++ academicgame.py
@@ -1,16 +1,21 @@
 import pygame
 import environment
+import hero
 
 
 class AcademicGame:
     def __init__(self,width,height):
         self.envShop = environment.ShopScene(width,height)
-        # self.envAction = environment.Scene(width,height)
+        self.envAction = environment.ActionScene(width,height)
         self.mWidth = width
         self.mHeight = height
+
+        
+
 
     def evolve(self,dt):
         pass
 
     def draw(self,surface):
         self.envShop.draw(surface)
+        self.envAction.draw(suface)
