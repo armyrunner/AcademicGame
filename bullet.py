6d3ee201d7da89b7d3c74a57efe56e5043063e97
kally1009 <d00372177@dmail.dixie.edu>
2020-11-13 20:32:27 -0700

    Bullet code

---
+++ bullet.py
@@ -0,0 +1,49 @@
+
+
+class Bullet:
+    def __init__(self,x,y,dx,dy,height,width):
+        self.mX=x
+        self.mY=y
+        self.mDx=dx
+        self.mDy=dy 
+        self.mScreenHeight=height
+        self.mScreenWidth=width
+    
+    def getX(self):
+        return self.mX
+    
+    def getY(self):
+        return self.mY
+    
+    def getDx(self):
+        return self.mDx
+    
+    def getDy(self):
+        return self.mDy
+    
+    def setX(self):
+        if self.mX>self.mScreenWidth:
+            self.mX=0
+        else:
+            self.mX+=self.mDx
+        
+    def setY(self):
+        if self.mY>self.mScreenHeight:
+            self.mY=0
+        else:
+            self.mY+=self.mDy
+    
+    def setDx(self,value):
+        self.mDx=value
+    
+    def setDy(self,value):
+        self.mDy=value
+    
+    
+    def drawBullet(self):
+        color=(1,1,1)
+        center=(self.mX,self.mY)
+        radius=5
+        draw.pygame.circle(surface, color, center, radius)
+    def draw(self,surface):
+        drawBullet(surface)
